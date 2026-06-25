78964726a1615067a4d0500e87af8a7b366f7b7d
syedadnanalamin <syedadnanhuda64@gmail.com>
2026-06-25 17:21:04 +0600

    server connected to the mongo

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+var cors = require('cors')
+const dotenv = require("dotenv");
+dotenv.config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const db = client.db("Teachers");
+        const teachersInfo = db.collection("teachersInfo");
+
+        app.get('/', async (req, res) => {
+            const projectFields = {
+
+                name: 1,
+                photo: 1,
+                subject: 1,
+                availableDays: 1,
+                hourlyFee: 1,
+                location: 1
+            }
+            const teachers = await teachersInfo.find().limit(6).project(projectFields).toArray();
+            res.send(teachers);
+
+        });
+
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
