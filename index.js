231cac26dd212838ba60062921eb62030f705e7d
syedadnanalamin <syedadnanhuda64@gmail.com>
2026-06-27 14:47:46 +0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-var cors = require('cors')
+var cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
@@ -21,6 +23,7 @@ async function run() {
         const db = client.db("Teachers");
         const teachersInfo = db.collection("teachersInfo");
         const BookedTeachers = db.collection("BookedTeachers");
+        const Mytutors = db.collection("Mytutors");
 
         app.get('/', async (req, res) => {
             const projectFields = {
@@ -58,11 +61,60 @@ async function run() {
             res.send(teachers);
 
         });
+
+        app.get("/my-booked-sessions/:id", async (req, res) => {
+            const UserId = req.params.id;
+
+            const BookedTutorByUser = await BookedTeachers.find({ UserId: UserId }).project({ tutorId: 1 }).toArray();
+            const tutorIds = BookedTutorByUser.map(
+                item => new ObjectId(item.tutorId)
+            );
+
+            const teacherDetails = await teachersInfo.find({
+                _id: { $in: tutorIds }
+            }).toArray();
+
+            res.send(teacherDetails);
+
+
+        })
+        app.delete("/my-booked-sessions/:id", async (req, res) => {
+            const id = req.params.id;
+
+
+            const BookedTutorByUser = await BookedTeachers.deleteOne({ tutorId: id });
+
+
+
+
+
+        })
+
+
         app.post('/tutors/details/:id', async (req, res) => {
             const doc = req.body;
             const result = await BookedTeachers.insertOne(doc);
 
         });
+
+
+        app.post("/add-tutor", async (req, res) => {
+            const tutor = req.body;
+
+            const result = await Mytutors.insertOne(tutor);
+
+            res.send(result);
+        });
+
+
+        app.get("/my-tutors/:id", async (req, res) => {
+            const userId = req.params.id;
+            const result = await Mytutors.find({ UserId: userId }).toArray();
+            console.log(result);
+            res.send(result);
+
+
+        })
 
     } finally {
 
